b09d3fea38be4512697fb25176ac6bcd8dcbc1af
Ravi Kondiparthi <ravi@oc1157324014.ibm.com>
2021-01-15 17:58:53 +0530

    added tags to eks cluster resource

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -69,6 +69,7 @@ resource "aws_eks_cluster" "eks_cluster" {
   name            = "${var.cluster_name}"
   role_arn        = "${aws_iam_role.cluster_role.arn}"
   version         = "${local.cluster_version}"
+  tags            = "${module.camtags.tagsmap}"
 
   vpc_config {
     security_group_ids = ["${aws_security_group.sg_cluster.id}"]
